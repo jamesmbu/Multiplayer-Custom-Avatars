99de3c15e49a78f278992218fcbe37ec3bfdbadc
James Burke <k1811535@kingston.ac.uk>
2021-04-23 18:42:23 +0100

    Spawn Zone 2: Nicer way to determine zone size
    a box component comes attached where the script for spawning uses the size of the box to determine the zone's breadth. This allows the zone size to simply be set in the inspector through adjusting the size opf the trigger box.

--- PlayerCustomisation/Assets/Scripts/Gameplay/SpawnObjects.cs
+++ PlayerCustomisation/Assets/Scripts/Gameplay/SpawnObjects.cs
@@ -6,15 +6,23 @@ public class SpawnObjects : MonoBehaviour
 {
     [SerializeField] private GameObject ObjectToSpawn;
 
-    public int xPos;
+    private float xPos;
 
-    public int zPos;
+    private float zPos;
 
+    private float X_Radius = 0;
+    private float Y_Radius = 0;
+    private float Z_Radius = 0;
+
+    private BoxCollider SpawnBounds;
     public int spawnedCounter;
     // Start is called before the first frame update
     void Start()
     {
-        
+        SpawnBounds = GetComponent<BoxCollider>();
+        X_Radius = SpawnBounds.size.x / 2;
+        Y_Radius = SpawnBounds.size.y / 2;
+        Z_Radius = SpawnBounds.size.z / 2;
     }
 
     void OnEnable()
@@ -36,9 +44,12 @@ public class SpawnObjects : MonoBehaviour
     {
         while (true)
         {
-            xPos = Random.Range(-15, 15);
-            zPos = Random.Range(15, 35);
-            Instantiate(ObjectToSpawn, new Vector3(xPos, 1, zPos), Quaternion.identity);
+            xPos = transform.position.x + Random.Range(X_Radius*-1, X_Radius);
+            zPos = transform.position.z + Random.Range(Z_Radius*-1, Z_Radius);
+
+            GameObject newObject = Instantiate(ObjectToSpawn, new Vector3(xPos, 1, zPos),
+                Quaternion.identity);
+
             yield return new WaitForSeconds(2f);
             spawnedCounter += 1;
         }
