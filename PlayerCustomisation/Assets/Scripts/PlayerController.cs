5a85a840ba1d7db7be23479012d27065807b73a7
oriconnage <60199545+oriconnage@users.noreply.github.com>
2021-04-24 08:43:11 +0100

    created a larger playing field and added boundaries

--- PlayerCustomisation/Assets/Scripts/PlayerController.cs
+++ PlayerCustomisation/Assets/Scripts/PlayerController.cs
@@ -4,51 +4,36 @@ using Photon.Pun;
 using UnityEngine;
 public class PlayerController : MonoBehaviourPun
 {
-    public float turnSpeed = 180;
-    public float tiltSpeed = 180;
-    public float walkSpeed = 1;
-
-
-   float maxHealth , Health;
-
-    [SerializeField]
-    private Transform fpcam;    // first person camera
-
-    private Rigidbody body;
-    [SerializeField]
-    private PhotonView View;
-
-
+    PlayerMaster Master;
+    PhotonView View;
     private void Awake()
     {
+        Master = GetComponentInParent<PlayerMaster>();
         View = GetComponent<PhotonView>();
     }
     // Start is called before the first frame update
     void Start()
     {
-        maxHealth = 100;
-        Health = maxHealth;
-      
-        // Cursor.lockState = CursorLockMode.Confined;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
         if (View.IsMine)
         {
-            
-            float forward = Input.GetAxis("Vertical");
-            float turn = Input.GetAxis("Horizontal") + Input.GetAxis("Mouse X");
-            float tilt = Input.GetAxis("Mouse Y");
-            transform.Translate(new Vector3(0, 0, forward * walkSpeed * Time.deltaTime));
-            transform.Rotate(new Vector3(0, turn * turnSpeed * Time.deltaTime, 0));
-            if (fpcam != null)
-                fpcam.Rotate(new Vector3(-tilt * tiltSpeed * Time.deltaTime, 0));
+            StartCoroutine("countdown");
         }
+        else 
+            Debug.Log("View not mine");
     }
 
+    IEnumerator countdown()
+    {
+        yield return new WaitForSeconds(2);
+        Master.CallEventModifyHealth(10);
+    }
 
 
 
